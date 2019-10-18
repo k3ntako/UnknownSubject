facbf5bc60ea75eb6bd047a63ba579c551d813da
k3ntako <kentarokaneki@gmail.com>
2019-10-17 23:59:25 -0400

    Add action CharacterList

--- client/models/CharacterList.js
+++ client/models/CharacterList.js
@@ -3,46 +3,103 @@ const CHARACTER_LIST = [
     id: "citizen",
     roleName: "Citizen",
     description: "As an average citizen you have no role, but make sure you are not wrongly convicted of the crime.",
-    role: null,
+    action: null,
   }, {
     id: "lookout",
     roleName: "Lookout",
     description: "You were watching from afar to make alert the robbers of any police officers. They have no solid evidence you were involved. You'll definitely win your appeal.",
-    role: null,
+    action: {
+      actionStage: 0,
+      action: 'view',
+      viewType: 'murderer',
+      select: false,
+    },
   }, {
     id: "witness",
     roleName: "Witness",
     description: "As a witness, they may have some information about what they saw.",
-    role: "Look at another person's card or view two cards from the middle."
+    action: {
+      actionStage: 0,
+      action: 'view',
+      select: {
+        selectUnassigned: 2,
+        selectAssigned: 1,
+        selectFromBoth: false,
+        selectSelf: false,
+        viewSelected: true,
+      },
+    },
   }, {
     id: "twin",
     roleName: "Twin",
-    description: "You have an alibi: you were with your twin."
+    description: "You have an alibi: you were with your twin.",
+    action: {
+      actionStage: 0,
+      action: 'view',
+      viewType: 'twin',
+      select: false,
+    },
   }, {
     id: "murderer",
     roleName: "Murderer",
     description: "You committed the crime. Make sure you don't get caught.",
-    role: "Make sure you know who were your accomplices.",
+    action: {
+      actionStage: 0,
+      action: 'view',
+      viewType: 'murderer',
+      select: false,
+    },
   }, {
     id: "scientist",
     roleName: "Scientist",
     description: "You created a machine that switches the body of two people.",
-    role: null,
+    action: {
+      actionStage: 0,
+      action: 'switch_other_assigned',
+      select: {
+        selectUnassigned: 2,
+        selectAssigned: 0,
+        selectFromBoth: false,
+        selectSelf: false,
+        viewSelected: false,
+      },
+    },
   }, {
     id: "grandparent",
     roleName: "Grandparent",
     description: "You always forget who you are.",
-    role: null,
+    action: {
+      action: 'switch_with_self',
+      select: {
+        selectUnassigned: 1,
+        selectAssigned: 0,
+        selectFromBoth: false,
+        selectSelf: false,
+        viewSelected: false,
+      },
+    },
   }, {
     id: "identity_thief",
     roleName: "Identity Thief",
     description: "You like to steal other people's roles.",
-    role: null,
+    action: {
+      action: 'switch_with_self',
+      select: {
+        selectUnassigned: 0,
+        selectAssigned: 1,
+        selectFromBoth: false,
+        selectSelf: false,
+        viewSelected: false,
+      },
+    },
   }, {
     id: "night_owl",
     roleName: "Night Owl",
     description: "You were up late, and you had the chance to check if your role has changed.",
-    role: null,
+    action: {
+      action: 'view_self',
+      select: false,
+    },
   }
 ];
 
